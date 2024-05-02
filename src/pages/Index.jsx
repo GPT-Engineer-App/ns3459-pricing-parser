a934514a1d90898a924e47e70c097bdd5a53c36f
Martin McGloin <martin.mcgloin@gmail.com>
2024-05-02 16:17:41 +0200

    test

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Button, Input, VStack, Text, Heading, Table, Thead, Tbody, Tr, Th, Td, Container, Alert, AlertIcon } from "@chakra-ui/react";
-import { FaUpload } from "react-icons/fa";
-import { parseStringPromise } from "xml2js";
+import { XMLParser } from 'fast-xml-parser';
+
 
 const Index = () => {
   const [file, setFile] = useState(null);
@@ -14,12 +14,21 @@ const Index = () => {
     if (file) {
       const reader = new FileReader();
       reader.onload = async (e) => {
+        const xml = e.target.result;
         try {
-          const xml = e.target.result;
-          const result = await parseStringPromise(xml);
-          setParsedData(result);
+          const parser = new XMLParser({
+            ignoreAttributes: false,
+            attributeNamePrefix: "@_",
+            textNodeName: "#text",
+            allowBooleanAttributes: true,
+          });
+          const result = parser.parse(xml);
+          await setParsedData(result); // Adjust this line based on how you process the XML
+          console.log(result);
+          console.log(parsedData?.NS3459?.Pristilbud?.ProsjektNS?.Poster);
           setError("");
         } catch (err) {
+          console.log(err);
           setError("Failed to parse XML file.");
         }
       };
@@ -27,33 +36,34 @@ const Index = () => {
     }
   };
 
-  // Removed parseXML function as xml2js is not allowed in project dependencies
-
   return (
     <Container maxW="container.xl">
       <VStack spacing={4} align="stretch" mt={10}>
         <Heading as="h1" size="xl">
-          NS3459 Pricing Converter
+          NS3459 Pricing Converter 2
         </Heading>
         <Text>Upload your NS3459 XML file to view the pricing details.</Text>
         <Input type="file" accept=".xml" onChange={handleFileChange} />
+        {parsedData && (<p>{parsedData.NS3459.Pristilbud.Generelt.ProgramNavn}</p>)}
         {parsedData && (
           <Table variant="simple">
             <Thead>
               <Tr>
                 <Th>Chapter</Th>
                 <Th>Description</Th>
-                <Th>Unit Price</Th>
-                <Th>Total</Th>
+                <Th>Mengderegel</Th>
+                <Th>Enhet</Th>
+                <Th>Mengde</Th>
               </Tr>
             </Thead>
             <Tbody>
-              {parsedData.NS3459.Pristilbud.Poster.Post.filter((post) => post.Postnrdeler.Postnrdel.some((nrdel) => nrdel.Type === "Type1" && nrdel.Kode === "36")).map((post, index) => (
+              {parsedData.NS3459?.Pristilbud?.ProsjektNS?.Poster?.Post.map((post, index) => (
                 <Tr key={index}>
                   <Td>{post.Postnr}</Td>
-                  <Td>{post.Tekst.Uformatert}</Td>
-                  <Td>{post.Prisinfo.Enhetspris}</Td>
-                  <Td>{post.Prisinfo.Sum}</Td>
+                  <Td>{post.Tekst?.Uformatert}</Td>
+                  <Td>{post.Prisinfo?.Mengderegel}</Td>
+                  <Td>{post.Prisinfo?.Enhet}</Td>
+                  <Td>{post.Prisinfo?.Mengde}</Td>
                 </Tr>
               ))}
             </Tbody>
@@ -64,29 +74,6 @@ const Index = () => {
             <AlertIcon />
             {error}
           </Alert>
-        )}
-        {parsedData && (
-          <Table variant="simple">
-            <Thead>
-              <Tr>
-                <Th>Chapter</Th>
-                <Th>Description</Th>
-                <Th>Unit Price</Th>
-                <Th>Total</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {/* Example of rendering parsed data */}
-              {parsedData.NS3459.Pristilbud.Poster.Post.map((post, index) => (
-                <Tr key={index}>
-                  <Td>{post.Postnr}</Td>
-                  <Td>{post.Tekst.Uformatert}</Td>
-                  <Td>{post.Prisinfo.Enhetspris}</Td>
-                  <Td>{post.Prisinfo.Sum}</Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
         )}
       </VStack>
     </Container>
